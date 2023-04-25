ed2428e6156bee7004396f856650ef9154ea39e3
Elderguard <111159021+Elderguard@users.noreply.github.com>
2023-04-25 19:51:16 -0300

    Update 18.type_of_triangle.cpp

--- 18.type_of_triangle.cpp
+++ 18.type_of_triangle.cpp
@@ -14,9 +14,9 @@ int main(){
 	cout<<"Inform the dimension of side C of the Triangle";
 	cin>>C;
 	
-	if (A=B=C)
+	if (A==B==C)
 		cout<<"This is an Equilateral triangle";
-	else if (A=B || B=C || C=A)
+	else if (A==B || B==C || C==A)
 		cout<<"This is an Isosceles triangle";
 	else 
 		cout<<"This is a Scalene triangle";
